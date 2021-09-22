fd9d15f404bcc4db645e5d7acfe3a3cd6e907382
Yousiph1 <yousiph77@gmail.com>
2021-09-22 13:02:21 +0000

    declared and parsed flag variables

---
+++ flags/flags.go
@@ -0,0 +1,22 @@
+package flags
+import (
+  "flag"
+)
+
+var MoviePath string
+var SubTitlePath string
+var Suffix string
+var Name string
+var Destination string
+var C bool
+
+
+func init(){
+ flag.StringVar(&MoviePath,"-moviepath","","path to movie files")
+ flag.StringVar(&SubTitlePath,"-subtitlepath","","path to subtitle files")
+ flag.StringVar(&Suffix,"-suffix","S0*E0*","example Seoson01Episode01 = Season0*Episode0*")
+ flag.StringVar(&Name,"-name","","movie name")
+ flag.StringVar(&Destination,"-d","movie path","destination folder to put both movie files and subtitle")
+ flag.BoolVar(&C,"-c",false,"move files or copy")
+ flag.Parse()
+}
